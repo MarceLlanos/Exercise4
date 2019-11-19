953c5b3840583afc028b13d57b2b86cc6ac9f7ff
Marcela Llanos <marcela3llanos@gmail.com>
2019-11-19 08:57:54 -0400

    Se arreglo la parte de las Listas

--- Program.cs
+++ Program.cs
@@ -10,10 +10,33 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            Console.Write("Input a name:");
-            String text;
-            text = Console.ReadLine();
-            Console.Write("Hola mundo {0}", text);
+            var people = new List<Person>();
+            //TODO: Add a number of person within the list
+            people.Add(new Person { Name = "Ester", LastName = "Quiroga" });
+            people.Add(new Person { Name = "Juan", LastName = "Reanult" });
+            people.Add(new Person { Name = "David", LastName = "Montanio" });
+            people.Add(new Person { Name = "Ivan", LastName = "Esparza" });
+            people.Add(new Person { Name = "Pedro", LastName = "Agno" });
+            people.Add(new Person { Name = "Karen", LastName = "Morales" });
+            people.Add(new Person { Name = "Veronica", LastName = "Jimenez" });
+            people.Add(new Person { Name = "Any", LastName = "Vargas" });
+            people.Add(new Person { Name = "Carla", LastName = "Trigo" });
+
+            PersonComparer personComparer = new PersonComparer();
+            people.Sort(personComparer);
+            int i = 0;
+            foreach (Person item in people)
+            {
+                
+                i++;
+                Console.WriteLine("Person {0}: {1} {2}",i, item.Name, item.LastName);
+                
+            }
+
+            
+            
+            //TODO: Print the sorted names in the console
+
             Console.ReadKey();
         }
     }
